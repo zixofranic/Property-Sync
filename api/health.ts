a2548ab97a9e2b19d163e8df77b8f608c13aecd3
Ziad El Feghali <ziadfeg@gmail.com>
2025-08-23 08:27:09 -0400

    added health file in API directory

---
+++ api/health.ts
@@ -0,0 +1,6 @@
+// File path: /pages/api/health.js
+
+export default function handler(req, res) {
+  // This function handles all requests to /api/health
+  res.status(200).send('OK');
+}
--- api/health/route.js
+++
@@ -1,6 +0,0 @@
-// File path: /app/api/health/route.js
-
-export async function GET(request) {
-  // This function responds to GET requests at /api/health
-  return new Response('OK', { status: 200 });
-}
